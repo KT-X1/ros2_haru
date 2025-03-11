30a365156f4692b8aa2b9b6f8f7e02dc56fa7f53
Altairu <106.nogu@gmail.com>
2025-03-11 14:32:07 +0900

    new

--- build/harurobo_pkg/prefix_override/sitecustomize.py
+++ build/harurobo_pkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/altair/ros2_haru/install/harurobo_pkg'
+    sys.prefix = sys.exec_prefix = '/home/altair/.local/share/Trash/files/ros2_haru/install/harurobo_pkg'
